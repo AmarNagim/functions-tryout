fcf3859edae39045db446779a690783781a22d4a
AmarNagim <99069493@mydavinci.nl>
2021-09-24 12:08:50 +0200

    hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Amar Nagim
+# Maak een bestand aan in je repo genaamd hello.py. Hier in define en call je een function.
+# De function moet een print() bevatten waar in staat “Hello from function town”
+
+def hello():
+    print('Hello from function town')
+
+hello()    
